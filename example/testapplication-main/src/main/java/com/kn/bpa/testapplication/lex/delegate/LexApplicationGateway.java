ca2b970f8775bac63b18b6f94df29e774298b036
Stefan Becke <stefan.becke@kuehne-nagel.com>
2019-05-03 11:46:43 +0200

    Introduced DMN for the lex decision gateway

--- example/testapplication-main/src/main/java/com/kn/bpa/testapplication/lex/delegate/LexApplicationGateway.java
+++ example/testapplication-main/src/main/java/com/kn/bpa/testapplication/lex/delegate/LexApplicationGateway.java
@@ -2,11 +2,17 @@ package com.kn.bpa.testapplication.lex.delegate;
 
 import static com.kn.bpa.lex.common.LexConstants.VARIABLE.LEX_CLIENT_APPLICATION_ID;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import org.camunda.bpm.engine.DecisionService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,20 +21,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LexApplicationGateway implements JavaDelegate {
 
+  private static final String DMN_FILE = "LEX_DECISION";
+  private static final String VAR_OFFICE = "office";
+  private static final String VAR_LEX_APPLICATION_ID = "lexClientApplicationId";
+  private static final String DMN_FALBACK = "UNDEFINED";
+
+
+  @Autowired
+  private DecisionService decisionService;
+
   @Override
   public void execute(final DelegateExecution execution) {
     log.info("Evaluate lex application");
-    if (execution.getVariable("office").toString().startsWith("DEKN_DEHAM"))
-    {
-      log.info("Configuration for a lex application found");
-      VariableMap variables = Variables.createVariables()
-        .putValueTyped(LEX_CLIENT_APPLICATION_ID, Variables.stringValue("LEX_EMEA_DE", true));
-      execution.setVariables(variables);
-    } else {
-      VariableMap variables = Variables.createVariables()
-        .putValueTyped(LEX_CLIENT_APPLICATION_ID, Variables.stringValue(StringUtils.EMPTY, true));
-      execution.setVariables(variables);
-      log.info("No configuration for a lex application found. - continue global process");
+    String lexApplicationID = evaluateAssignmentDecisionTableWithContext(evaluateDecisionTableInput(execution.getVariable("office").toString()), DMN_FILE, VAR_LEX_APPLICATION_ID);
+
+    log.info("Lex application is " + lexApplicationID + " for businessId " + execution.getBusinessKey() + " and activity " + execution.getActivityInstanceId());
+    VariableMap variables = Variables.createVariables()
+        .putValueTyped(LEX_CLIENT_APPLICATION_ID, Variables.stringValue(lexApplicationID, true));
+    execution.setVariables(variables);
+  }
+
+  private Map<String, Object> evaluateDecisionTableInput(@Nonnull String officeName) {
+    Map<String, Object> vars = new HashMap<>();
+    vars.put(VAR_OFFICE,officeName);
+    return vars;
+  }
+
+  private String evaluateAssignmentDecisionTableWithContext(final Map<String, Object> context, final String dmnKey, final String resultName) {
+    final Map<String, Object> result = decisionService.evaluateDecisionTableByKey(dmnKey, context).getSingleResult();
+    String resultStr =  DMN_FALBACK;
+    if (result != null && result.containsKey(resultName)) {
+      resultStr = (String) result.get(resultName);
     }
+    return resultStr;
   }
 }
